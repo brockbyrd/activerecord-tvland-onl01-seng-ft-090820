b840dd6fec58091501d542d954915a15edf4ba2b
Brock Byrd <brockbyrdd@gmail.com>
2020-10-13 18:58:23 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,1 +1,9 @@
-class Create
+class CreateCharacters < ActiveRecord::Migration[5.1]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+    end
+  end
+end
